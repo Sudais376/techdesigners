e18308f66f164191402e9671fabb7f82b652938c
Muhammad Sudais <sudais376376@gmail.com>
2024-11-15 10:09:39 -0800

    Add files via upload

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,69 +1,70 @@
-import React from "react";
-
 const ContactForm = () => {
+  const handleSendMessage = (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const contact = document.getElementById("email").value;
+    const message = document.getElementById("textarea").value;
+
+    const whatsappMessage = `https://wa.me/923335101246?text=Hello!%0A%0AMy%20Name:%20${encodeURIComponent(
+      name
+    )}%0AContact:%20${encodeURIComponent(
+      contact
+    )}%0A%0AHow%20Can%20We%20Help%20You:%20${encodeURIComponent(message)}`;
+
+    window.open(whatsappMessage, "_blank");
+  };
+
   return (
-    <>
-      <div
-        className="w-[80vw] mx-auto bg-gradient-to-r from-[#806d9a] to-[#493763] pt-24 pb-12 px-8 text-white flex flex-col gap-5 rounded-[40px] border-2 border-transparent bg-clip-border 
-      shadow-[inset_0_0_0_2px_#e81cff,inset_0_0_0_2px_#40c9ff]"
+    <div className="w-[80vw] mx-auto bg-gradient-to-r from-[#806d9a] to-[#493763] pt-24 pb-12 px-8 text-white flex flex-col gap-5 rounded-[40px] border-2 border-transparent bg-clip-border shadow-[inset_0_0_0_2px_#e81cff,inset_0_0_0_2px_#40c9ff]">
+      <form
+        onSubmit={handleSendMessage}
+        className="flex flex-col gap-5"
       >
-        <form data-aos ="fade-up"  className="flex flex-col gap-5">
-          {/* Name Input */}
-          <div data-aos ="fade-up"  className="flex flex-col gap-1">
-            
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              required
-               data-aos ="fade-up" className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] 
-              placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
-            />
-          </div>
-          {/* Email Input */}
-          <div data-aos ="fade-up"  className="flex flex-col gap-1">
-            
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Email / Phone number"
-              required
-               data-aos ="fade-up" className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] 
-              placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
-            />
-          </div>
-
-          {/* Textarea */}
-          <div data-aos ="fade-up"  className="flex flex-col gap-1">
-            <label
-              htmlFor="textarea"
-               data-aos ="fade-up" className="text-white font-semibold text-lg"
-            >
-              How Can We Help You?
-            </label>
-            <textarea
-              id="textarea"
-              name="textarea"
-              rows="5"
-              required
-               data-aos ="fade-up" className="w-full p-3 rounded-lg text-white bg-[#493763] border border-[#414141] 
-              focus:outline-none focus:border-[#e81cff] resize-none"
-            ></textarea>
-          </div>
-
-          {/* Submit Button */}
-          <div data-aos ="fade-up" className=" flex justify-center items-center ">
+        <div className="flex flex-col gap-1">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            required
+            className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email / Phone number"
+            required
+            className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="textarea"
+            className="text-white font-semibold text-lg"
+          >
+            How Can We Help You?
+          </label>
+          <textarea
+            id="textarea"
+            name="textarea"
+            rows="5"
+            required
+            className="w-full p-3 rounded-lg text-white bg-[#493763] border border-[#414141] focus:outline-none focus:border-[#e81cff] resize-none"
+          ></textarea>
+        </div>
+        <div className="flex justify-center items-center">
           <button
-                className=" w-[40vw] btn1 !px-0 !m-0 transition ease-in duration-500"
-              >
-                Send message
-              </button>
-              </div>
-        </form>
-      </div>
-    </>
+            type="submit"
+            className="w-[40vw] btn1 !px-0 !m-0 transition ease-in duration-500"
+          >
+            Send message
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,40 +10,41 @@ const Footer = () => {
 
   return (
     <>
-      <section className=" relative z-20 py-12 bg-[#491b77] max-w-[1800px] mx-auto">
+      <section className=" relative z-20 py-12 bg-[#491b77] max-w-[1800px] mx-auto px-6">
         <div
           data-aos=""
           className="flex flex-col lg:flex-row justify-around items-center gap-2"
         >
-           <div className="pt-8 flex flex-col items-end ">
-              <span className=" font-bold self-center text-[#ff4df3] pb-4 ">Design Services</span>
-            <div className=" flex items-center gap-3 py-3">
+           <div className="pt-8 flex flex-col items-start lg:items-end border-b border-white lg:border-b-0 ">
+              <span className=" font-bold text-[#ff4df3] pb-4 ">Design Services</span>
+            <div className=" flex items-center gap-3 py-3 flex-row-reverse lg:flex-row ">
                 
               <a href="mailto:personal.sudais376@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.% What%Will%be%the%process?" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500"> personl.sudais376@gmail.com </span> </a>
               <a href="mailto:personal.sudais376@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.% What%Will%be%the%process?" target="_blank">
-                <img src="/assets/gmail.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/gmail.svg" width={40} alt="" />
               </a>
               
               </div>
-              <div className=" flex items-center gap-3 py-3">
+              <div className=" flex items-center gap-3 py-3 flex-row-reverse lg:flex-row ">
                 
               <a href="https://wa.me/+923335101246" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500">  +92 333 5101246  </span> </a>
               <a href="https://wa.me/+923335101246" target="_blank">
-                <img src="/assets/whatsapp.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/whatsapp.svg" width={40} alt="" />
               </a>
               
               </div>
-              <div className=" flex items-center gap-3 py-3">
+              <div className=" flex items-center gap-3 py-3 flex-row-reverse lg:flex-row ">
                 
               <a href="https://github.com/Sudais376" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500"> github.com/Sudais376 </span> </a>
               <a href="https://github.com/Sudais376" target="_blank">
-                <img src="/assets/github.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/github.svg" width={40} alt="" />
               </a>
               
               </div>
             </div>
+            
           <div>
-            <div className="pt-8">
+            <div className="pt-8 pb-8 lg:pb-0 ">
               <ul className="flex flex-wrap justify-center items-center gap-8 lg:pr-16 px-8 ">
                 <Link
                   to="/"
@@ -84,26 +85,26 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div>
-            <div className="pt-8 flex flex-col  ">
-              <span className=" font-bold text-center text-[#ff4df3] pb-4 ">Web Solutions</span>
+          <div className=" border-t border-white lg:border-t-0 ">
+            <div className=" pt-2 lg:pt-8 flex flex-col  ">
+              <span className=" font-bold text-[#ff4df3] pb-4 ">Web Solutions</span>
             <div className=" flex items-center gap-3 py-3">
                 <a href="mailto:personal.sudais376@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.% What%Will%be%the%process?" target="_blank">
-                <img src="/assets/gmail.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/gmail.svg" width={40} alt="" />
               </a>
               <a href="mailto:personal.sudais376@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.% What%Will%be%the%process?" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500"> personl.sudais376@gmail.com </span> </a>
               
               </div>
               <div className=" flex items-center gap-3 py-3">
                 <a href="https://wa.me/+923335101246" target="_blank">
-                <img src="/assets/whatsapp.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/whatsapp.svg" width={40} alt="" />
               </a>
               <a href="https://wa.me/+923335101246" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500">  +92 333 5101246  </span> </a>
               
               </div>
               <div className=" flex items-center gap-3 py-3">
                 <a href="https://github.com/Sudais376" target="_blank">
-                <img src="/assets/github.svg" width={40} alt="" />
+                <img className=" hidden xl:block " src="/assets/github.svg" width={40} alt="" />
               </a>
               <a href="https://github.com/Sudais376" target="_blank"><span className="  hover:text-[#ff4df3] transition duration-500"> github.com/Sudais376 </span> </a>
               
