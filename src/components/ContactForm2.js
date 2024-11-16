66669548b9bbb602eefd5646042d19fad43a4706
Muhammad Sudais <sudais376376@gmail.com>
2024-11-16 08:27:16 -0800

    Add files via upload

--- src/components/ContactForm2.js
+++ src/components/ContactForm2.js
@@ -42,52 +42,50 @@ const ContactForm2 = () => {
     <>
       <div className="w-full max-w-[500px] mx-auto bg-gradient-to-r from-[#806d9a] to-[#493763] py-6 px-8 text-white flex flex-col gap-5 rounded-[20px] border-2 border-transparent bg-clip-border shadow-[inset_0_0_0_2px_#e81cff,inset_0_0_0_2px_#40c9ff]">
         {/* Attach the form reference */}
-        <form ref={form} onSubmit={handleSubmit} className="flex flex-col gap-5">
-          <div>
-            <span>
-              Please feel free to describe what you need, We will reach you out
-              and try to solve your problem.
-            </span>
-          </div>
-          <div className="flex flex-col gap-1">
+        <form
+          ref={form}
+          onSubmit={handleSubmit} // Form submission handler
+          data-aos ="fade-up" className="flex flex-col gap-5"
+        >
+          <div data-aos ="fade-up" className="flex flex-col gap-1">
             <input
               type="text"
-              id="name"
-              name="name"
+              id="from_name"
+              name="from_name" // This will match the template variable {{from_name}}
               placeholder="Name"
               required
-              className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
+              data-aos ="fade-up" className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
             />
           </div>
-          <div className="flex flex-col gap-1">
+          <div data-aos ="fade-up" className="flex flex-col gap-1">
             <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Email / Phone number"
+              type="email"
+              id="from_email"
+              name="from_email" // This will match the template variable {{from_email}}
+              placeholder="Email"
               required
-              className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
+              data-aos ="fade-up" className="w-full p-3 rounded-lg text-lg text-white bg-[#493763] border border-[#857898] placeholder:opacity-50 focus:outline-none focus:border-[#e81cff]"
             />
           </div>
-          <div className="flex flex-col gap-1">
+          <div data-aos ="fade-up" className="flex flex-col gap-1">
             <label
-              htmlFor="textarea"
-              className="text-white font-semibold text-lg"
+              htmlFor="message"
+              data-aos ="fade-up" className="text-white font-semibold text-lg"
             >
               How Can We Help You?
             </label>
             <textarea
-              id="textarea"
-              name="textarea"
+              id="message"
+              name="message" // This will match the template variable {{message}}
               rows="5"
               required
-              className="w-full p-3 rounded-lg text-white bg-[#493763] border border-[#414141] focus:outline-none focus:border-[#e81cff] resize-none"
+              data-aos ="fade-up" className="w-full p-3 rounded-lg text-white bg-[#493763] border border-[#414141] focus:outline-none focus:border-[#e81cff] resize-none"
             ></textarea>
           </div>
           <div className="flex justify-center items-center">
             <button
               type="submit"
-              className="w-full btn1 px-4 py-2 bg-[#e81cff] text-white font-bold rounded-lg hover:bg-[#40c9ff] transition-all ease-in-out duration-500"
+              className="w-[40vw] btn1 !px-0 !m-0 transition ease-in duration-500"
             >
               Send message
             </button>
--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Thanks = () => {
   return (
     <>
-    <div>This is Thanks</div>
+    <div className=' flex flex-col justify-center items-center '>
+      <div className=' flex flex-col justify-center items-center '>
+        <img src="/assets/success.svg" width={50} alt="" />
+        <span className=' py-4 text-2xl '>Thanks</span>
+      </div>
+      <div className=' text-center '>
+        <span className=' text-center '>Your message was submitted successfully, We will get back to you soon.</span>
+      </div>
+
+    </div>
     </>
   )
 }
